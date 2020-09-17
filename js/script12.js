7ef91bbcede80c9a94d78949a49ee8de39fa0fb5
Denis <denissdudarevs@gmail.com>
2020-09-17 03:42:34 +0100

    first code

---
+++ js/script12.js
@@ -0,0 +1,17 @@
+'use strict';
+const numberOfFilms = +prompt('How many movies did you wached?', '');
+
+const   personalMovieOB = {
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false
+};
+
+const   a = prompt('The last wached movie?', ''),
+        b = prompt('How you will rate it?', ''),
+        c = prompt('The last wached movie?', ''),
+        d = prompt('How you will rate it?', '');
+
+personalMovieOB.movies[a] =b;
