33353c95979b6da2ace12141f29e8a30333f367c
Denis <denissdudarevs@gmail.com>
2020-09-17 02:16:14 +0100

    first commit

---
+++ script7.js
@@ -0,0 +1,19 @@
+"use strict";
+
+//alert('Hello');
+ 
+//const result = confirm("Are you here?");
+//console.log(result);
+
+// const answer = +prompt("Are you 18 years old?", "18");
+// console.log(typeof(answer));
+// console.log(answer + 5);
+
+const answer = [];
+
+// answer[0] = prompt('What is your name?', '');
+// answer[1] = prompt('What is your surname?', '');
+// answer[2] = prompt('How old are you?', '');
+
+console.log(typeof(answer));
+//console.log(typeof(null));
